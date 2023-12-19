53eb8391c60284f090f821bbcf3820051f99ab10
SANAYEE <dudtkd0219@gmail.com>
2023-12-19 20:30:00 +0900

    초기 작업

---
+++ src/config/AppRouter.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./index.css";
+import App from "./App";
+import Login from "./component/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Copyright() {
+    return (
+        <div variant="body2" color="textSecondary" align="center">
+            {" Copyright c"}
+            ysp, {new Date().getFullYear()}
+            {"."}
+        </div>
+    );
+}
+
+function AppRouter(){
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                    <Route path="login" element={<Login />} />
+                </Routes>
+            </BrowserRouter>
+            <div mt={5}>
+                <Copyright />
+            </div>
+        </div>
+    )
+}
+
+export default AppRouter;
